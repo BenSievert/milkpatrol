78a73d21c273444cd7a1651d7b5c230ea73735df
Ben Sievert <bsievert@precisionplanting.com>
2023-11-12 17:23:55 -0600

    milk it up

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from "./routes/home";
+import Home from "./routes/Home";
 import FAQ from "./routes/FAQ";
 import About from "./routes/About";
 import Support from "./routes/Support";
@@ -11,18 +11,25 @@ const links = [
   { title: `Home`, path: `/`, component: Home },
   { title: `FAQ`, path: `faq`, component: FAQ },
   { title: `About`, path: `about`, component: About },
-  {title: `Support`, path: `support`, component: Support }
+  { title: `Support`, path: `support`, component: Support },
 ];
 ReactDOM.createRoot(document.getElementById(`root`)!).render(
   <div className="p-6 text-gray-900">
     <BrowserRouter basename={import.meta.env.BASE_URL}>
       <div>
         {links.map((link) => (
-          <Link className="mr-2 underline hover:bg-gray-100 p-1 rounded" to={link.path}>{link.title}</Link>
+          <Link
+            className="mr-2 underline hover:bg-gray-100 p-1 rounded"
+            to={link.path}
+          >
+            {link.title}
+          </Link>
         ))}
       </div>
       <Routes>
-        {links.map((link) => <Route element={<link.component />} path={link.path} />)}
+        {links.map((link) => (
+          <Route element={<link.component />} path={link.path} />
+        ))}
       </Routes>
     </BrowserRouter>
   </div>
--- src/routes/Support.tsx
+++ src/routes/Support.tsx
@@ -1,7 +1,7 @@
 function Support() {
   return (
     <div className="text-center">
-      Give us money, I swear itll go towards the music
+      Give us money, We swear itll go towards the music
       [insert patreon, paypal, zelle, cashapp, venmo, phishing scam]
     </div>
   );

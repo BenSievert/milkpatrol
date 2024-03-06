bba0bb3a61088a673eb7b0424d62ad4af5c52d07
Ben Sievert <bsievert@precisionplanting.com>
2024-03-06 13:12:35 -0600

    mobile nav

--- src/main.tsx
+++ src/main.tsx
@@ -11,17 +11,17 @@ import { Link } from "react-router-dom";
 const links = [
   { title: `Home`, path: `/`, component: Home },
   { title: `Schedule`, path: `schedule`, component: Schedule },
-  { title: `FAQ`, path: `faq`, component: FAQ },
   { title: `About`, path: `about`, component: About },
+  { title: `FAQ`, path: `faq`, component: FAQ },
   { title: `Support`, path: `support`, component: Support },
 ];
 ReactDOM.createRoot(document.getElementById(`root`)!).render(
   <div className="p-6 text-gray-900">
     <BrowserRouter basename={import.meta.env.BASE_URL}>
-      <div className="mb-6 bg-pink-600 py-4 px-6 rounded-full inline-block">
+      <div className="mb-6 bg-pink-600 py-4 px-6 rounded-2xl md:rounded-full inline-block w-full">
         {links.map((link) => (
           <Link
-            className="mr-2 underline hover:bg-pink-400 p-2 rounded-2xl font-bold text-white"
+            className="text-sm mr-2 hover:bg-pink-400 px-2 p-1 md:p-2 rounded-2xl font-bold text-white block md:inline-block"
             to={link.path}
           >
             {link.title}

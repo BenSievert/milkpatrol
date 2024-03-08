70ba1f4858356db71d546141de7e33f30ec0702e
Ben Sievert <bsievert@precisionplanting.com>
2024-03-08 11:28:40 -0600

    milkify

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,15 @@ import "./index.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import {links} from "./components/NavBar";
 import  NavBar from './components/NavBar'
+import CowPrint from "./components/CowPrint.tsx";
 
 ReactDOM.createRoot(document.getElementById(`root`)!).render(
   <div className="p-2 md:p-6 bg-gradient-to-br from-violet-200 to-pink-500 min-h-screen font-sans text-gray-900">
     <BrowserRouter basename={import.meta.env.BASE_URL}>
-      <NavBar />
+      <div className='relative z-10'><NavBar /></div>
       <div className='text-center'>
-      <div className='text-left bg-pink-100 p-4 md:p-16 rounded-3xl border-2 border-black shadow-2xl inline-block'>
+        <CowPrint />
+      <div className='text-left bg-pink-100 p-4 md:p-16 rounded-3xl border-2 border-black shadow-2xl inline-block relative z-10'>
       <Routes>
         {links.map((link, i ) => (
           <Route key={i} element={<div className='flex justify-center text-sm md:text-xl'>

4fbe434d4b948a3f481caa8fac85b1fc1740b978
Ben Sievert <bsievert@precisionplanting.com>
2024-04-10 13:14:12 -0500

    wip

--- src/routes/Schedule.tsx
+++ src/routes/Schedule.tsx
@@ -4,9 +4,18 @@ function Schedule() {
   document.title = `Milk Patrol-Schedule`
   return (
     <div>
-      <div className='text-2xl mb-24'>
-        Upcoming: <span className='text-base'>None :( Please book us</span>
+      <div className='text-2xl mb-4'>
+        Upcoming:
       </div>
+      <Card>
+        <div className='mb-3 text-xl'>
+          <span className='font-bold'>Where:</span> The Bone Student Center
+          <br></br>
+          <span className='font-bold'>When:</span> 7pm, April 17th, 2024
+        </div>
+        <div className='flex justify-center align-middle'>
+          <a className='border-2 border-black rounded' href="https://www.instagram.com/p/C5ghuJthe1u/"><img width='200px' alt='no image :(' src="https://i.imgur.com/7hZnFZ7.png" title="source: imgur.com" /></a></div>
+      </Card>
       <div>
         <div className='text-2xl mb-4'>  Previous:</div>
 

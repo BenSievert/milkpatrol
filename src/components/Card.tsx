76bd9514b61a106478f1e30ed73c45429c289980
Ben Sievert <bsievert@precisionplanting.com>
2024-08-09 10:41:38 -0500

    fix card width

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -6,7 +6,7 @@ function Card({children} : any) {
   const context =  useContext(ThemeContext).card;
 
   return (
-    <div className={`rounded-2xl p-2 md:p-6 ${context.background} ${context.border} inline-block border-2  shadow-2xl`}>
+    <div className={`w-full rounded-2xl p-2 md:p-6 ${context.background} ${context.border} inline-block border-2  shadow-2xl`}>
       {children}
     </div>
   )
